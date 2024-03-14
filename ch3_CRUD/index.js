c68a2aa0526deffe9f39a6df76464748c2be41ca
Viery Nugroho <viery15102002@gmail.com>
2024-03-14 23:16:18 +0700

    update: CRUD & Code Refactoring

--- ch3_CRUD/index.js
+++ ch3_CRUD/index.js
@@ -3,55 +3,180 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 const { randomUUID } = require('crypto');
 
-//! config
+//! ------------- config -------------
 const app = express();
 dotenv.config();
 
-//! Middleware
+//! ------------- Middleware -------------
 app.use(express.json());
 
-//! declaration var config
+//! ------------- declaration var config -------------
 const PORT = process.env.PORT;
 
-//! service
-const getAll = (filename) => {
-	const data = JSON.parse(fs.readFileSync(`${__dirname}/public/data/${filename}`));
+//! ------------- service ------------------
+const getAll = () => {
+	const data = JSON.parse(fs.readFileSync(`${__dirname}/public/data/data.json`));
+
 	return data;
 };
 
-const insertCustomer = (filename, data) => {
-	fs.writeFileSync(`${__dirname}/public/data/${filename}`, JSON.stringify(data));
+const getById = (id) => {
+	const data = getAll().find((cust) => cust._id === id);
+	if (!data) {
+		throw Error('Customer not found!');
+	}
+
 	return data;
 };
 
-//! route
-app.get('/', (req, res, next) => {
-	res.send('Hi World');
-});
+const writeFile = (data) => {
+	fs.writeFileSync(`${__dirname}/public/data/data.json`, JSON.stringify(data));
+	return data;
+};
 
-app.get('/api/v1/customers', (req, res, next) => {
-	res.status(200).json({
-		status: 'OK',
-		length: getAll('data.json').length,
-		data: getAll('data.json'),
-	});
-});
+//! ------------- Controller -------------
+const getRoot = (req, res, next) => {
+	res.send('Welcome tuh API akuuuuh');
+};
 
-app.post('/api/v1/customers', (req, res, next) => {
-	const customers = getAll('data.json');
-	const data = req.body;
-	data.id = randomUUID();
+const getAllData = (req, res) => {
+	try {
+		const data = getAll();
 
-	customers.push(data);
+		res.status(200).json({
+			status: 'OK',
+			length: data.length,
+			data: data,
+		});
+	} catch (error) {
+		res.status(500).json({
+			status: 'FAIL',
+			message: `${error}`,
+		});
+	}
+};
 
-	insertCustomer('data.json', customers);
+const getDataById = (req, res) => {
+	try {
+		const { id } = req.params;
+		const customer = getById(id);
 
-	res.status(201).json({
-		status: 'OK',
-		length: getAll('data.json').length,
-		data,
-	});
-});
+		res.status(200).json({
+			status: 'OK',
+			data: customer,
+		});
+	} catch (error) {
+		res.status(404).json({
+			status: 'FAIL',
+			message: `${error}`,
+		});
+	}
+};
+
+const createData = (req, res) => {
+	try {
+		const customers = getAll();
+		const data = req.body;
+		data._id = randomUUID();
+
+		customers.push(data);
+
+		writeFile(customers);
+
+		res.status(201).json({
+			status: 'OK',
+			length: getAll().length,
+			data,
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: 'FAIL',
+			message: `${error}`,
+		});
+		res.status(500).json({
+			status: 'FAIL',
+			message: `${error}`,
+		});
+	}
+};
+
+const patchData = (req, res) => {
+	try {
+		const { id } = req.params;
+		//TODO: validation id customer
+		getById(id);
+
+		const customers = getAll();
+		const customerIndex = customers.findIndex((cust) => cust._id === id);
+
+		customers[customerIndex] = { ...customers[customerIndex], ...req.body };
+
+		writeFile(customers);
+
+		res.status(200).json({
+			status: 'OK',
+			message: 'Customer Updated!',
+			data: {
+				customer: customers[customerIndex],
+			},
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: 'FAIL',
+			message: `${error}`,
+		});
+		res.status(500).json({
+			status: 'FAIL',
+			message: `${error}`,
+		});
+	}
+};
+
+const deleteData = (req, res) => {
+	try {
+		const { id } = req.params;
+		//TODO: validation id customer
+		getById(id);
+
+		const customersUndeleted = getAll().filter((customer) => customer._id !== id);
+		writeFile(customersUndeleted);
+
+		res.status(200).json({
+			status: 'success',
+			message: 'Customer deleted!',
+			length: customersUndeleted.length,
+			data: customersUndeleted,
+		});
+	} catch (error) {
+		res.status(500).json({
+			status: 'FAIL',
+			message: `${error}`,
+		});
+	}
+};
+
+//! ------------- routes -------------
+app.get('/', getRoot);
+
+//TODO: get all data
+app.get('/api/v1/customers', getAllData);
+
+//TODO: get data by ID
+app.get('/api/v1/customers/:id', getDataById);
+
+//TODO: post data
+app.post('/api/v1/customers', createData);
+
+//? PUT / PATCH ?
+//* PUT
+// update semua data
+//* PATCH
+// update data yang diperlukan
+//TODO: patch data
+app.patch('/api/v1/customers/:id', patchData);
+
+//TODO: delete data
+app.delete('/api/v1/customers/:id', deleteData);
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
